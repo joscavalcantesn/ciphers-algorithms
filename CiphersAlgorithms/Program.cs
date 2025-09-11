e60597e9561a409c1868a254cf8b894bd80d7cab
joscavalcantesn <joscavalcantesn@gmail.com>
2025-09-11 08:41:01 -0300

    add VernamCipher support in Program.cs

--- CiphersAlgorithms/Program.cs
+++ CiphersAlgorithms/Program.cs
@@ -2,6 +2,8 @@
 
 var caesarCipher = new CaesarCipher();
 var vigenereCipher = new VigenereCipher();
+var vernamCipher = new VernamCipher();
 
 caesarCipher.Run();
 vigenereCipher.Run();
+vernamCipher.Run();
